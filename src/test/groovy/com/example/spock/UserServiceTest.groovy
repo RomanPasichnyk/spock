4d0cb690d2fb345fc090624f10339645868caad2
Roman Pasichnyk <roman.pasichnyk@globallogic.com>
2020-03-13 08:34:19 +0200

    change User name from Roman to Romann add test name length test method

--- src/test/groovy/com/example/spock/UserServiceTest.groovy
+++ src/test/groovy/com/example/spock/UserServiceTest.groovy
@@ -13,14 +13,24 @@ class UserServiceTest extends Specification {
 
     def "check method find by name"() {
         given:
-        userRepository.findUserByName("Roman") >> new User("Roman", 15)
-        def expectedUser = new User("Roman change in service", 15)
+        userRepository.findUserByName("Romann") >> new User("Romann", 15)
+        def expectedUser = new User("Romann change in service", 15)
 
         when:
-        def actualUser = userService.findUserByName("Roman")
+        def actualUser = userService.findUserByName("Romann")
 
         then:
         expectedUser == actualUser
-        1 * userRepository.findUserByName(_) >> new User("Roman", 15)
+        1 * userRepository.findUserByName(_) >> new User("Romann", 15)
+    }
+
+    @Unroll("#name should have length #length")
+    def "name length"() {
+        expect:
+        name.size() == length
+
+        where:
+        name << ["Kirk", "Spock", "Scotty"]
+        length << [4, 5, 6]
     }
 }
