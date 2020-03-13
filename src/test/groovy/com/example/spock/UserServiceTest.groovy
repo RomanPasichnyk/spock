8ab87b97201446de4c7195214b5b457af2de4b77
Roman Pasichnyk <roman.pasichnyk@globallogic.com>
2020-03-13 08:27:19 +0200

    merge two tests in one - UserServiceTest

--- src/test/groovy/com/example/spock/UserServiceTest.groovy
+++ src/test/groovy/com/example/spock/UserServiceTest.groovy
@@ -23,25 +23,4 @@ class UserServiceTest extends Specification {
         expectedUser == actualUser
         1 * userRepository.findUserByName(_) >> new User("Roman", 15)
     }
-
-//    @Unroll
-//    def "check amount of call method in repository from service"() {
-//        when:
-//        userService.findUserByName(_ as String)
-//
-//        then:
-//        1 * userRepository.findUserByName(_) >> new User("Roman", 152)
-//    }
-
-    @Unroll("#name should have length #length")
-    def "name length"() {
-        expect:
-        name.size() == length
-
-        where:
-        name << ["Kirk", "Spock", "Scotty"]
-        length << [4, 5, 6]
-    }
-
-
 }
